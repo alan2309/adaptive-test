c52592481f05b6380afb781818aa218dd378172a
Chaitanya <kchaitanya1911@gmail.com>
2021-11-29 17:41:30 +0530

    AddedQsViews

--- server/api/models.py
+++ server/api/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Questions(models.Model):
     title = models.CharField(max_length=255)
     type = models.IntegerField()
+    def __str__(self):
+        return self.title
 
 class Options(models.Model):
     question = models.ForeignKey(Questions,on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     marks = models.IntegerField()  
+    def __str__(self):
+        return self.title +' '+ self.question
--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-
-from api.models import Questions
+from django.http.response import HttpResponse, JsonResponse
+from api.models import Questions,Options
 
 # Create your views here.
 def qs(request):
+    a=[]
+    c=[]
+    b=[]
     if request.method =='GET':
         qs = Questions.objects.all()
+        for x in qs:
+            aa={}
+            aaOption=[]
+            aa['ques']=x.title
+            ans = Options.objects.filter(question=x)
+            for asss in ans:
+                aaaOpt={}
+                aaaOpt['opt']=asss.title
+                aaaOpt['mrks']=asss.marks
+                aaOption.append(aaaOpt)
+            
+            aa['options']=aaOption
+            if x.type==1:
+                a.append(aa)
+            elif x.type==2:
+                b.append(aa)
+            elif x.type==3:
+                c.append(aa)
+            print(a)
+            print(b)
+            print(c)
+
+                
+    return JsonResponse({'easy':a,'medium':b,'hard':c},safe=False)
