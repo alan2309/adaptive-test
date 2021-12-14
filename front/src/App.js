bf3484ec92248eb8d8c7ab8cdb395e54f22eadfe
Alankrit Arya <alankritarya15@gmail.com>
2021-12-14 22:28:05 +0530

    basescreen removed

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,49 @@
-import BaseScreen from './screens/BaseScreen';
+import React from 'react'
+import TestScreen from './screens/TestScreen'
+import Home from './screens/Home'
+import logo from './img/logo.png'
+import {Col,Row} from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
     <div className="App">
-      <BaseScreen/>
+       <div>
+           
+
+
+           <Row style={{height:'100px',margin:'0px'}}>
+               <Col md={1}>
+           <img style={{ width:"60px",height:"75px",marginLeft:'56px'}}
+           src={logo}
+           alt="logo"
+           className="d-inline-block align-middle mr-2"
+         />
+         </Col>
+         <Col md={11} style={{paddingTop:'10px',paddingLeft:'40px',paddingRight:'0px'}}>
+             <Row style={{margin:'0px'}}>
+         DJSCE INFORMATION TECHNOLOGY
+         </Row>
+         <Row style={{margin:'0px'}}>
+         Placement Portal
+         </Row>
+         </Col>
+         </Row>
+         <Row style={{margin:'0px'}}>
+
+          <Col style={{padding:'10px 90px'}} >
+          <Router>
+       <Routes>
+       <Route path='/testScreen' exact element={<TestScreen/>}/>
+       <Route path='/' exact element={<Home/>} />
+       <Route path='/login' exact element={<Login/>} />
+       </Routes>
+       </Router>
+           
+           </Col>
+           </Row>
+       </div>
     </div>
   );
 }
