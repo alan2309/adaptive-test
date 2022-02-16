7ae32143db3cbdb297a71ed7683bb49523e199b9
Chaitanya <kchaitanya1911@gmail.com>
2022-02-16 13:16:23 +0530

    addedLabel

--- front/src/components/TestScreeen/keys.js
+++ front/src/components/TestScreeen/keys.js
@@ -1,8 +1,8 @@
 function key() {
   let a = [
-    `d35b7822fcmshab047f684dc27bap12bb13jsnfc29849e5539`,
-    `86a2155308msh040dc3e8039f9dfp1ffa84jsn538591614eec`,
-    `1dc9039649msh93f4945ac9964cdp172d36jsnc0c3f9a06290`,
+    `d35b7822fcmshab047f684dc27bap12bb13jsnfc29849e5539`, //ck
+    `86a2155308msh040dc3e8039f9dfp1ffa84jsn538591614eec`,//kchai
+    `1dc9039649msh93f4945ac9964cdp172d36jsnc0c3f9a06290`,//alankrit
   ];
   return a;
 }
