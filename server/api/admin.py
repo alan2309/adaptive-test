d334a555abf5b87f05f273e16c5d2d108597a6b0
Chaitanya <kchaitanya1911@gmail.com>
2022-02-17 17:36:42 +0530

    solvedOutStandingBug

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import (CodingTest, MyUser, Questions,Options,Results,Subject,Test,Paraqs,Paraopt,Para,MyUser)
+from rest_framework_simplejwt import token_blacklist
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin,OutstandingToken
 
 # Register your models here.
 admin.site.register(Questions)
@@ -11,5 +13,10 @@ admin.site.register(CodingTest)
 admin.site.register(Paraqs)
 admin.site.register(Paraopt)
 admin.site.register(Para)
-
 admin.site.register(MyUser)
+
+class OutstandingTokenAdmin(OutstandingTokenAdmin):
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+admin.site.unregister(OutstandingToken)
+admin.site.register(OutstandingToken, OutstandingTokenAdmin)
